f320ee0bf23542ae5c635d51ef701842a13f34af
rhyloo <rhyloo@juntx>
2023-12-28 23:09:44 +0100

    Update org-tidy and C-e move toend line

--- init.el
+++ init.el
@@ -28,7 +28,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(markdown-command "/usr/bin/markdown" t)
- '(org-agenda-files '("~/org/notes.org"))
+ '(org-agenda-files nil)
  '(package-selected-packages
    '(org-tidy ox-hugo org-static-blog org2blog magit-pretty-graph elmacro org-gtasks mu4e counsel arduino-cli-mode platformio-mode undo-tree cmake-mode ws-butler which-key websocket weblorg web-server vscode-dark-plus-theme vs-dark-theme use-package treemacs swiper super-save sr-speedbar solaire-mode slime simple-httpd scihub pyvenv python-mode popup pdf-tools ox-reveal org-special-block-extras org-roam org-ref org-present org-noter org-make-toc org-gcal org-fragtog neotree multiple-cursors mu4e-alert minions matlab-mode markdown-preview-eww lua-mode lsp-ui lsp-ltex ledger-mode json-mode guess-language fountain-mode forge flycheck exec-path-from-shell epresent edit-indirect doom-modeline deft company-arduino benchmark-init auctex-latexmk all-the-icons))
  '(safe-local-variable-values '((eval add-hook 'after-save-hook 'org-babel-tangle)))
