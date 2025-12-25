7a063dbd70d751cd7a36d2a003b0e4cdb8814b33
rhyloo <rhyloot@gmail.com>
2025-12-25 13:12:57 +0100

    Update: New track file .gitignore

---
+++ early-init.el
@@ -0,0 +1,25 @@
+;;; early-init.el --- Emacs early init file for Emacs 30.2.5  -*- lexical-binding: t -*-
+;; Company:       RhylooSolutions
+;; Engineer:      Rhyloo
+;; 
+;; Create Date:   2025-12-25
+;; Project Name:  Personal Emacs configuration
+;; Version:       0.0.1v
+;;
+;;; Description:
+;;
+;; This is the main configuration file for Emacs.
+;; In this file:
+;; - Disable package.el manager
+;;
+;;; Revision:
+;;
+;; Revision 0.0.1 - File Created
+;;
+;;; Additional Comments:
+;;
+;; -----------------------------------------------------------------------------
+
+;;; Code:
+(setq package-enable-at-startup nil) ;; Disable package.el in favor of elpaca.el
+;;; early-init.el ends here
